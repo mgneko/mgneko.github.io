f2733b3c317db8217d36806c9a57725a1129d972
leaflu0315 <johnson7511333@gmail.com>
2023-09-05 17:20:01 +0800

    TW GDGD

--- init.js
+++ init.js
@@ -56,8 +56,8 @@ var AllCategoryNUM = {
 	servents['rider'].length, servents['caster'].length, servents['assassin'].length, servents['berserker'].length,
 	servents['ruler'].length, servents['avenger'].length, servents['alterego'].length, servents['foreigner'].length, servents['mooncancer'].length, servents['pretender'].length,servents['beast'].length],
 	// 台服
-	"tw": [15, 11, 11,
-		13, 13, 10, 12,
+	"tw": [15, 11, 12,
+		13, 14, 10, 12,
 		7, 6, 6, 7, 3, 1, 0],
 	// 自選
 	"z":  [3,4,4,6,5,3,4,1,0,1,0,1,0],
