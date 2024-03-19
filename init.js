36a25c6cbd5a76eae02db1d30dcedec90eb3c086
leaflu0315 <johnson7511333@gmail.com>
2024-03-19 10:48:26 +0800

    台 2.6.5

--- init.js
+++ init.js
@@ -58,9 +58,9 @@ var AllCategoryNUM = {
 	servents['rider'].length, servents['caster'].length, servents['assassin'].length, servents['berserker'].length,
 	servents['ruler'].length, servents['avenger'].length, servents['alterego'].length, servents['foreigner'].length, servents['mooncancer'].length, servents['pretender'].length,servents['beast'].length,servents['shielder'].length],
 	// 台服
-	"tw": [16, 11, 12,
+	"tw": [17, 11, 12,
 		15, 14, 10, 12,
-		7, 6, 8, 8, 3, 1, 0],
+		8, 6, 8, 8, 3, 1, 0],
 	// 自選
 	"z":  [3,4,4,6,5,3,4,1,0,1,0,1,0],
 	// 七周年
